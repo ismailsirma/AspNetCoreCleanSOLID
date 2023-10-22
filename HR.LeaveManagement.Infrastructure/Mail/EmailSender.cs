742e23746268879e8407d26e7b09c26b86f32141
Ismail Erdem Sirma <ismailsirma@gmail.com>
2023-10-22 13:27:20 +0300

    Implement email external service infrastructure layer

---
+++ HR.LeaveManagement.Infrastructure/Mail/EmailSender.cs
@@ -0,0 +1,29 @@
+﻿using HrLeaveManagement.Application.Contracts.Infrastructure;
+using HrLeaveManagement.Application.Models;
+using Microsoft.Extensions.Options;
+using System;
+using System.Threading.Tasks;
+
+namespace HR.LeaveManagement.Infrastructure.Mail
+{
+    public class EmailSender : IEmailSender
+    {
+        private EmailSettings _emailSettings { get; }
+        public EmailSender(IOptions<EmailSettings> emailSettings)
+        {
+            _emailSettings = emailSettings.Value;
+        }
+        public async Task<bool> SendEmail(Email email)
+        {
+            /*
+            var client = new SendGridClient(_emailSettings.ApiKey);
+            var to = new EmailAddress(email.To)
+            var from = new EmailAddress { Email = _emailSettings.FromAddress, Name = _emailSettings.FromName };
+            var message = MailHelper.CreateSingleEmail(from, to, email.Subject, email.Body, email.Body);
+            var response = await client.SendEmailAsync(message);
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+            */
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ HrLeaveManagement.Application/Contracts/Infrastructure/IEmailSender.cs
@@ -0,0 +1,13 @@
+﻿using HrLeaveManagement.Application.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HrLeaveManagement.Application.Contracts.Infrastructure
+{
+    public interface IEmailSender
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
