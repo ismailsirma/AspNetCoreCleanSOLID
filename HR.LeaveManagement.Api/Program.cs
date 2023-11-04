d3a3c14e1558555a5d66087ad1020eb4e5eb77cd
Ismail Erdem Sirma <ismailsirma@gmail.com>
2023-11-04 21:09:15 +0300

    Fix project configurations

--- HR.LeaveManagement.Api/Program.cs
+++ HR.LeaveManagement.Api/Program.cs
@@ -27,7 +27,7 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HR.LeaveManagement.Api v1"));
 }
 
 app.UseHttpsRedirection();
