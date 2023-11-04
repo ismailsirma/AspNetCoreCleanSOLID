c7649c6021f139bf7de1909cb36a031fda3b83e1
Ismail Erdem Sirma <ismailsirma@gmail.com>
2023-11-04 20:40:25 +0300

    Add new thin controller using mediatr library to direct requests to command handlers

---
+++ HR.LeaveManagement.Api/Controllers/LeaveTypesController.cs
@@ -0,0 +1,63 @@
+﻿using HrLeaveManagement.Application.DTOs.LeaveType;
+using HrLeaveManagement.Application.Features.LeaveRequests.Requests.Commands;
+using HrLeaveManagement.Application.Features.LeaveTypes.Requests.Commands;
+using HrLeaveManagement.Application.Features.LeaveTypes.Requests.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HR.LeaveManagement.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LeaveTypesController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public LeaveTypesController(IMediator mediator) 
+        { 
+            _mediator = mediator;
+        }
+        // GET: api/<LeaveTypesController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<LeaveTypeDto>>> Get()
+        {
+            var leaveTypes = await _mediator.Send(new GetLeaveTypeListRequest());
+            return leaveTypes;
+        }
+
+        // GET api/<LeaveTypesController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<LeaveTypeDto>> Get(int id)
+        {
+            var leaveType = await _mediator.Send(new GetLeaveTypeDetailRequest { Id = id});
+            return Ok(leaveType);
+        }
+
+        // POST api/<LeaveTypesController>
+        [HttpPost]
+        public async Task<ActionResult> Post([FromBody] CreateLeaveTypeDto leaveType)
+        {
+            var command = new CreateLeaveTypeCommand { LeaveTypeDto = leaveType };
+            var response = await _mediator.Send(command);
+            return Ok(response);
+        }
+
+        // PUT api/<LeaveTypesController>/5
+        //[HttpPut("{id}")]
+        public async Task<ActionResult> Put([FromBody] LeaveTypeDto leaveType)
+        {
+            var command = new UpdateLeaveTypeCommand { LeaveTypeDto = leaveType };
+            await _mediator.Send(command);
+            return NoContent(); // http 204
+        }
+
+        // DELETE api/<LeaveTypesController>/5
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(int id)
+        {
+            await _mediator.Send(new DeleteLeaveTypeCommand { Id = id });
+            return NoContent();
+        }
+    }
+}
---
+++ HrLeaveManagement.Application/Features/LeaveTypes/Requests/Commands/CreateLeaveTypeCommand.cs
@@ -0,0 +1,12 @@
+﻿using HrLeaveManagement.Application.DTOs.LeaveRequest;
+using HrLeaveManagement.Application.DTOs.LeaveType;
+using HrLeaveManagement.Application.Responses;
+using MediatR;
+
+namespace HrLeaveManagement.Application.Features.LeaveRequests.Requests.Commands
+{
+    public class CreateLeaveTypeCommand : IRequest<BaseCommandResponse>
+    {
+        public CreateLeaveTypeDto LeaveTypeDto { get; set; }
+    }
+}
